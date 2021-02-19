538545bd893e68cfef43efce769baa178131b596
hpdnnr7 <hpdnnr7@gmail.com>
2021-02-19 17:13:17 -0500

    final changes for part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Hope Dunner on 2/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+}
---
+++ flixster/MoviesDetailsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesDetailsViewController.swift
+//  flixster
+//
+//  Created by Hope Dunner on 2/19/21.
+//
+
+import UIKit
+
+class MoviesDetailsViewController: UIViewCell {
+
+}
